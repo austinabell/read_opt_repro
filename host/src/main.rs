6c7d05b998d959f324dc7123f4d983ae4e437f8b
Austin Abell <austinabell8@gmail.com>
2023-09-14 11:34:16 -0400

    base example with serializing input

---
+++ host/src/main.rs
@@ -0,0 +1,31 @@
+// TODO: Update the name of the method loaded by the prover. E.g., if the method
+// is `multiply`, replace `METHOD_NAME_ELF` with `MULTIPLY_ELF` and replace
+// `METHOD_NAME_ID` with `MULTIPLY_ID`
+use methods::{METHOD_NAME_ELF, METHOD_NAME_ID};
+use risc0_zkvm::serde::{from_slice, to_vec};
+use risc0_zkvm::{default_prover, ExecutorEnv};
+
+fn main() {
+    let bytes = vec![8u8; 400];
+    // First, we construct an executor environment
+    let env = ExecutorEnv::builder()
+        .add_input(&to_vec(&bytes).unwrap())
+        .build()
+        .unwrap();
+
+    // Obtain the default prover.
+    let prover = default_prover();
+
+    // Produce a receipt by proving the specified ELF binary.
+    let receipt = prover.prove_elf(env, METHOD_NAME_ELF).unwrap();
+
+    // TODO: Implement code for transmitting or serializing the receipt for
+    // other parties to verify here
+    let cycles: u32 = from_slice(&receipt.journal).unwrap();
+    println!("cycles: {}", cycles);
+    assert_eq!(cycles, 31253);
+
+    // Optional: Verify receipt to confirm that recipients will also be able to
+    // verify your receipt
+    receipt.verify(METHOD_NAME_ID).unwrap();
+}
---
+++ methods/guest/src/main.rs
@@ -0,0 +1,20 @@
+#![no_main]
+// If you want to try std support, also update the guest Cargo.toml file
+#![no_std] // std support is experimental
+
+extern crate alloc;
+
+use alloc::vec::Vec;
+use core::hint::black_box;
+use risc0_zkvm::guest::env;
+
+risc0_zkvm::guest::entry!(main);
+
+pub fn main() {
+    let start = env::get_cycle_count();
+    let _data: Vec<u8> = black_box(env::read());
+    let end = env::get_cycle_count();
+
+    let cycles = (end - start) as u32;
+    env::commit(&cycles);
+}
